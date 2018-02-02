46292ddff1c33d0229aa103afb8ca7102b112465
jencekade <gaephuoc@gmail.com>
2018-02-02 08:09:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array) {
+  let index = 0
+  array.forEach(function (element){
+    console.log(`${index}: ${element}`);
+    index++;
+  });
+}
+
+function iterate() {
+  
+}
+
+function doToArray() {
+  
+}
